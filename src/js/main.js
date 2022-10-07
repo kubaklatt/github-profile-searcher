3311795d722f8e4cb407bdbcb5872fdf3e2f61aa
Kuba <jakubklatt@icloud.com>
2022-10-08 00:53:23 +0200

    js ready

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+const input = document.querySelector('#input')
+const searchBtn = document.querySelector('#search')
+const alertText = document.querySelector('.alert-text')
+const avatar = document.querySelector('.avatar')
+const userFullName = document.querySelector('.name')
+const userLogin = document.querySelector('.login')
+const userDate = document.querySelector('.date')
+const userBio = document.querySelector('.bio-paragraph')
+const userRepos = document.querySelector('.repos-amount')
+const userFollowers = document.querySelector('.followers-amount')
+const userFollowing = document.querySelector('.following-amount')
+const userLocation = document.querySelector('.user-location')
+const userWebsite = document.querySelector('.user-website')
+const userTwitter = document.querySelector('.user-twitter')
+const userCompany = document.querySelector('.user-company')
+let userName
+
+searchBtn.addEventListener('click', () => {
+	userName = input.value
+	fetch('https://api.github.com/users/' + userName)
+		.then(res => {
+			if (res.ok) {
+				return res.json()
+			} else {
+				return Promise.reject(`HTTP ERROR: ${res.status}`)
+			}
+		})
+		.then(res => {
+			alertText.style.visibility = 'hidden'
+
+			const resDate = res.created_at.slice(0, res.created_at.length - 10)
+			const [year, month, day] = resDate.split('-')
+			const result = [month, day, year].join('/')
+
+			avatar.setAttribute('src', res.avatar_url)
+			userFullName.innerHTML = res.name
+			userLogin.innerHTML = '@' + res.login
+			userDate.innerHTML = 'Joined ' + result
+			userBio.innerHTML = res.bio
+			userRepos.innerHTML = res.public_repos
+			userFollowers.innerHTML = res.followers
+			userFollowing.innerHTML = res.following
+			userLocation.innerHTML = res.location === null ? 'Not Available' : res.location
+			userWebsite.innerHTML = res.blog === '' ? 'Not Available' : res.blog
+			userTwitter.innerHTML = res.twitter_username === null ? 'Not Available' : res.twitter_username
+			userCompany.innerHTML = res.company === null ? 'Not Available' : res.comapny
+		})
+		.catch(error => {
+			console.error(error)
+			alertText.style.visibility = 'visible'
+		})
+})
